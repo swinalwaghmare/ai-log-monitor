66a37403443d9fa3c1c229e0fc5e10234b8c22ef
Swinalwaghmare <swinalwaghmare28@gmail.com>
2026-04-05 18:16:32 +0530

    prometheus added

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from prometheus_client import Counter, generate_latest
+
 from app.routes import logs
 from app.config import settings
 
-app = FastAPI(title=settings.APP_NAME, version=settings.VERSION)
+app = FastAPI(
+    title=settings.APP_NAME, 
+    version=settings.VERSION
+    )
+
+# Prometheus metric
+REQUEST_COUNT = Counter(
+    "request_count",
+    "Total number of requests"
+)
 
 # include routes
 app.include_router(logs.router)
 
 @app.get("/")
 def root():
+    REQUEST_COUNT.inc()  # increment metric
     return {
         "message": "AI Log Monitoring Backend Running 🚀"
     }
 
+
+@app.get("/metrics")
+def metrics():
+    """
+    Prometheus will scrape this endpoint
+    """
+    return Response(
+        content=generate_latest(),
+        media_type="text/plain"
+    )
